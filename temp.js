a8e48d35e952772e7245158379d443b23ccf1c95
Rajarshee Chatterjee <rajarshee.adm@gmail.com>
2025-10-13 11:02:43 +0530

    Cleanup

--- temp.js
+++
@@ -1,9 +0,0 @@
-// scripts/build-favicon.ts
-import { writeFileSync } from "node:fs";
-import sharp from "sharp";
-
-sharp("src/app/icon.svg")
-  .resize(256, 256) // ICOs are bitmap; pick sizes you want
-  .toFormat("ico")
-  .toBuffer()
-  .then((buffer) => writeFileSync("src/app/favicon.ico", buffer));
